33846e4949b9d589c7bbbdbe9c7b96f97b26ceda
Jhonnathan93 <jsocampod@eafit.edu.co>
2024-01-27 19:14:54 -0500

    added descriptive comments

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,18 @@
 <?php
 
+// Database configuration
+// DSN stands for Data Source Name: provides the details necessary for establishing a connection to a data source.
 $dsn = "mysql:host=localhost;dbname=php_test";
 $dbusername = "root";
 $dbpassword = "";
 
 try{
+    // Create a new PDO (PHP data object) instance for database connection
+    // It is a PHP extension that provides a uniform method of access to multiple databases.
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    // Set PDO attributes to handle errors using exceptions
+    // PDO::ATTR_ERRMODE is an attribute of the PHP Data Objects (PDO) extension that determines how PDO should handle errors.
+    // PDO::ERRMODE_EXCEPTION: This mode causes PDO to throw exceptions when errors occur.
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     echo "Conection failed: " . $e->getMessage();
--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -5,10 +5,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $lastname = $_POST["lastname"];
 
     try {
+        // Include the database connection file
         require_once "dbh.inc.php";
 
         $query = "INSERT INTO users (name, lastname) VALUES (:name, :lastname);";
 
+        // Prepared statements are a way to execute SQL queries with placeholders for parameters. 
+        // This helps prevent SQL injection attacks.
         $stmt = $pdo->prepare($query);
 
         $stmt->bindParam(":name", $name);
@@ -16,11 +19,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         $stmt->execute();
 
+        // Close the database connection and the prepared statement
         $pdo = null;
         $stmt = null;
-
+        
         header("Location: ../index.php");
 
+        // Terminate the script to prevent further execution
         die();
     } catch (PDOException $e) {
         die("Error: " . $e->getMessage());
